505eb1ac3f17dc250a0aba55bfa31e37da8190c1
huynh <huynhric@gmail.com>
2026-09-22 23:26:39 -0400

    commit 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { DogBreed } from './shared/models/dog-breed';
 @Component({
   imports: [RouterOutlet],
   selector: 'app-root',
@@ -8,6 +8,49 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
 })
 export class App {
-  id: number = 16;
-  name: String = "Sam";
+  protected title: String = 'Kind of Dogs';
+
+  protected dogList: DogBreed[] = [
+    {
+      name: 'Border Collie',
+      colour: 'white and tan',
+      id: 7,
+      weight: 70,
+      hypoallergenic: true,
+    },
+    {
+      name: 'Dog2',
+      colour: 'blue',
+      id: 5,
+      weight: 80,
+      hypoallergenic: true,
+    },
+    {
+      name: 'Boggie',
+      colour: 'red',
+      id: 11,
+      weight: 120,
+      hypoallergenic: false,
+    },
+    {
+      name: 'Pitbull',
+      colour: 'white and black',
+      id: 18,
+      weight: 50,
+    },
+    {
+      name: 'Dog3',
+      colour: 'tan',
+      id: 87,
+      weight: 23,
+      hypoallergenic: false,
+    },
+    {
+      name: 'Terrier',
+      colour: 'brown',
+      id: 45,
+      weight: 42,
+      hypoallergenic: true,
+    },
+  ];
 }
---
+++ src/app/shared/models/dog-breed.ts
@@ -0,0 +1,7 @@
+export interface DogBreed {
+  name: String;
+  colour: String;
+  id: number;
+  weight:number;
+  hypoallergenic?: boolean
+}
